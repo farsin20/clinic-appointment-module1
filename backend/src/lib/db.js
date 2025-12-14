d7134c266f4972ac37e1097d8542e2fd7ce29d72
farsin20 <farsin.nahin@gmail.com>
2025-12-14 12:37:26 +0600

    Fix: Move MongoDB credentials to environment variables for security

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,9 +1,8 @@
 import mongoose from "mongoose";
 
-
 export const connectDB = async ()=>{
   try{
-    await mongoose.connect('mongodb+srv://sonusaha573:Sst123@cluster0.7vfw4q0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("Database connected");
     
   }catch(e){
